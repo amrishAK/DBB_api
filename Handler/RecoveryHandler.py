b2be14885910bce409659395dce31847465ad621
amrishAK <aamrish007@live.com>
2020-04-16 12:22:06 +0100

    bug fix recvory handler

--- Handler/RecoveryHandler.py
+++ Handler/RecoveryHandler.py
@@ -6,26 +6,29 @@ import json
 class RecoveryHandler:
 
     def __init__(self):
-        config = JsonHandler().LoadJson('Config.json')
-        print(config)
-        Client = pymongo.MongoClient("mongodb://localhost:27017/")
-        dataBase = Client['DBB_db']
-        PostCollection = dataBase['PostCollection']
-        postList = []
-        cursor = PostCollection.find({})
+        try:
+            config = JsonHandler().LoadJson('Config.json')
+            print(config)
+            Client = pymongo.MongoClient("mongodb://localhost:27017/")
+            dataBase = Client['DBB_db']
+            PostCollection = dataBase['PostCollection']
+            postList = []
+            cursor = PostCollection.find({})
 
-        for doc in cursor:
-            postList.append(doc)
+            for doc in cursor:
+                postList.append(doc)
 
-        sortedList = sorted(postList, key = lambda i : i['PayLoadToken'], reverse=True)
-        print(str(len(sortedList)))
-        token = sortedList[0]['PayLoadToken'] if len(sortedList) > 0 else 0
-        payload = {	"PayLoadToken" : token}
-        client = http.client.HTTPConnection(config['CordinatorHost'],config['CordinatorHTTPPort'])   
-        client.request('POST',"/recovery",json.dumps(payload),{'Content-type': 'application/json'})
-        response = client.getresponse()
-        if response.status == 200:
-            responseData = response.read()
-            responseJSon = json.loads(responseData)
-            if len(responseJSon) > 0:
-                PostCollection.insert_many(responseJSon)
+            sortedList = sorted(postList, key = lambda i : i['PayLoadToken'], reverse=True)
+            print(str(len(sortedList)))
+            token = sortedList[0]['PayLoadToken'] if len(sortedList) > 0 else 0
+            payload = {	"PayLoadToken" : token}
+            client = http.client.HTTPConnection(config['CordinatorHost'],config['CordinatorHTTPPort'])   
+            client.request('POST',"/recovery",json.dumps(payload),{'Content-type': 'application/json'})
+            response = client.getresponse()
+            if response.status == 200:
+                responseData = response.read()
+                responseJSon = json.loads(responseData)
+                if len(responseJSon) > 0:
+                    PostCollection.insert_many(responseJSon)
+        except Exception as ex:
+            print(ex)
